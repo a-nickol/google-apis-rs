880415f6153fd686f9a00faab71a47684a57bee5
a-nickol <a.nickol@trust-nickol.de>
2024-06-07 20:35:15 +0200

    url safe

--- gen/gmail1/src/api.rs
+++ gen/gmail1/src/api.rs
@@ -1056,6 +1056,7 @@ pub struct Message {
     pub payload: Option<MessagePart>,
     /// The entire email message in an RFC 2822 formatted and base64url encoded string. Returned in `messages.get` and `drafts.get` responses when the `format=RAW` parameter is supplied.
 
+    //#[serde_as(as = "Option<::client::serde::urlsafe_base64::Wrapper>")]
     pub raw: Option<Vec<u8>>,
     /// Estimated size in bytes of the message.
     #[serde(rename = "sizeEstimate")]
